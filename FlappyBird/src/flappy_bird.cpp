b7c1f8c732461359f2b99b3c221f69f663c13b81
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-06-07 00:03:52 -0300

    Fixed extern declarations

--- FlappyBird/src/flappy_bird.cpp
+++ FlappyBird/src/flappy_bird.cpp
@@ -1,15 +1,15 @@
 #include "flappy_bird.hpp"
 
-SURGE_MODULE_EXPORT auto on_load(GLFWwindow *) noexcept -> int { return 0; }
+extern "C" SURGE_MODULE_EXPORT auto on_load(GLFWwindow *) noexcept -> int { return 0; }
 
-SURGE_MODULE_EXPORT auto on_unload(GLFWwindow *) noexcept -> int { return 0; }
+extern "C" SURGE_MODULE_EXPORT auto on_unload(GLFWwindow *) noexcept -> int { return 0; }
 
-SURGE_MODULE_EXPORT auto draw(GLFWwindow *) noexcept -> int { return 0; }
+extern "C" SURGE_MODULE_EXPORT auto draw(GLFWwindow *) noexcept -> int { return 0; }
 
-SURGE_MODULE_EXPORT auto update(GLFWwindow *, double) noexcept -> int { return 0; }
+extern "C" SURGE_MODULE_EXPORT auto update(GLFWwindow *, double) noexcept -> int { return 0; }
 
-SURGE_MODULE_EXPORT void keyboard_event(GLFWwindow *, int, int, int, int) noexcept {}
+extern "C" SURGE_MODULE_EXPORT void keyboard_event(GLFWwindow *, int, int, int, int) noexcept {}
 
-SURGE_MODULE_EXPORT void mouse_button_event(GLFWwindow *, int, int, int) noexcept {}
+extern "C" SURGE_MODULE_EXPORT void mouse_button_event(GLFWwindow *, int, int, int) noexcept {}
 
-SURGE_MODULE_EXPORT void mouse_scroll_event(GLFWwindow *, double, double) noexcept {}
+extern "C" SURGE_MODULE_EXPORT void mouse_scroll_event(GLFWwindow *, double, double) noexcept {}
